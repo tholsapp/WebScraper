dca1ca8c137f5b0c266c4e332e18fdfee00ca8f0
Troy Holsapple <troy.holsapple@gmail.com>
2018-02-13 12:30:20 -0800

    Currently can scrape all pages with url links. Next step will be to step into each url and extract the names from the photo captions

---
+++ scraper/data_models.py
@@ -0,0 +1,67 @@
+
+from datetime import date
+
+month = {
+    'January'   : 1,
+    'February'  : 2,
+    'March'     : 3,
+    'April'     : 4,
+    'May'       : 5,
+    'June'      : 6,
+    'July'      : 7,
+    'August'    : 8,
+    'September' : 9,
+    'October'   : 10,
+    'November'  : 11,
+    'December'  : 12,
+}
+
+
+class URLDataStructure:
+  url = ''
+  published = date.today()
+
+  def __init__(self, url, y, m, d):
+    self.url = url
+    published = date(int(y), month[m], int(d))
+
+  def get_url(self):
+    return self.url
+
+  def get_day(self):
+    return self.published.day
+
+  def get_month(self):
+    return self.published.month
+
+  def get_year(self):
+    return self.published.year
+
+  def get_date(self):
+    return '%d-%d-%d' % (self.published.month, self.published.day, self.published.year)
+
+  def set_day(self, day):
+    self.day = day
+
+  def set_month(self, month):
+    self.month = month
+
+  def set_year(self, year):
+    self.year = year
+
+  def is_valid_url(self):
+    """ Requirements specify to not crawl postings prior
+        to Decemeber 1, 2014. """
+    if self.published < date(2014, 12, 1):
+      return True
+    else:
+      return False
+
+
+
+class PersonDataStructure:
+  first_name = ''
+  last_name = ''
+
+  def __init__(self):
+    print "Printing from class PersonalData"
